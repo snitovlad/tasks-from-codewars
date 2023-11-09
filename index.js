3d54f9a0f9f0841e0f687f8ed42ac220e64abe36
snitovlad <snitovov@gmail.com>
2023-11-09 18:52:06 +0300

    Task 2

--- index.js
+++ index.js
@@ -23,3 +23,13 @@ function isPangram(string){
    } 
    return false
  }
+
+
+ function squareDigits(num){
+   //7kyi
+   //Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+   //For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+   //Note: The function accepts an integer and returns an integer.
+   let x = String(num).split("").map(j => j*j)
+   return Number(x.join(""))
+ }

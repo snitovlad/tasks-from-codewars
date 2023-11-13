1c50e40875161ceefdf30538463e43f5110ef5d0
snitovlad <snitovov@gmail.com>
2023-11-14 00:11:48 +0300

    Task 7

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ function squareDigits(num) {
    return Number(x.join(""))
 }
 
+function nbYear(p0, percent, aug, p) {
+   //7kyi
+ let n = 0;
+ while (p0 < p) {
+   p0 = p0 + Math.floor(p0 * percent / 100) + aug;
+   n++;
+ }
+ return n;
+}
+
 
 function sumTwoSmallestNumbers(numbers) {
    //7kyi

9a5667de8accb06da88d9690afc8844511aae41e
snitovlad <snitovov@gmail.com>
2023-11-14 23:34:41 +0300

    Task 9

--- index.js
+++ index.js
@@ -88,3 +88,20 @@ function solution(number) {
    }
    return sum;
 }
+
+//task 9
+function likes(names) {
+   //6kyi
+   switch (names.length) {
+       case 0:
+       return `no one likes this`; //если бы не было return был бы нужен break
+       case 1:
+       return `${names[0]} likes this`;
+       case 2:
+       return `${names[0]} and ${names[1]} like this`;
+       case 3:
+       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+       default:
+       return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+   }
+ }
